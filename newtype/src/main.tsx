3da2ad05b078f2c0c447fe9a2844c9f5ef51da0c
sawa-zen <sawadasuiren@gmail.com>
2025-05-11 22:10:01 +0900

    fixup! fixup! fixup! newtype

--- newtype/src/main.tsx
+++ newtype/src/main.tsx
@@ -1,18 +1,24 @@
+import { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from "@react-three/drei";
 import { App } from './App'
 import './styles.css'
-import { Suspense } from 'react';
+import { createXRStore, XR, XROrigin } from '@react-three/xr';
+
+const store = createXRStore()
 
 createRoot(document.getElementById('root')!).render(
   <Canvas
     gl={{ alpha: true, antialias: true, pixelRatio: 1 }}
     camera={{ position: [0, 0, 1], fov: 120 }}
   >
-    <App />
-    <Suspense>
-      <OrbitControls />
-    </Suspense>
+    <XR store={store}>
+      <XROrigin position={[0, 0, 1]} />
+      <App />
+      <Suspense>
+        <OrbitControls />
+      </Suspense>
+    </XR>
   </Canvas>,
 )
